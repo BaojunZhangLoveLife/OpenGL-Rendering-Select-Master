5380b6588fd8ad6b08913cd7e5897e6708b91dbc
zhangbaojun <89057244+BaojunZhangLoveLife@users.noreply.github.com>
2023-03-07 09:11:56 +0800

    1

--- DataProcessing.cpp
+++ DataProcessing.cpp
@@ -388,7 +388,12 @@ void DataProcessing::translateModel(QPoint& point, QMatrix4x4& model, QMatrix4x4
 	modelSave = modelSave * modelUse;
 }
 
-std::vector<float> DataProcessing::test(std::string oriPlyPath, std::string transMeshPlyPath, std::string transMeshPcdPath, std::string finalMeshPath) {
+std::vector<float> DataProcessing::test(
+									std::string oriPlyPath,
+									std::string transMeshPlyPath,
+									std::string transMeshPcdPath, 
+									std::string finalMeshPath) 
+{
 	ply2ply(oriPlyPath, transMeshPlyPath);
 	ply2pcd(transMeshPlyPath, transMeshPcdPath);
 	getNormalVector(transMeshPcdPath);
